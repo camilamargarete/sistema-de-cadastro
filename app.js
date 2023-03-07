b20d854a10bdad00a1765c4f60871ee58e9324da
Camila__Candido <97892869+camilamargarete@users.noreply.github.com>
2023-03-07 12:20:58 -0300

    console.log indicando modificações de paulo

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
 console.log("Rodando o sistema de gerenciamento de jogos")
 console.log("a aplicação está se conectando ao banco de dados")
 //mais um console log de teste:
-console.log("O paulo fez esse commit sem você ver na aula")
+console.log("O paulo fez esse commit sem você ver na aula.")
